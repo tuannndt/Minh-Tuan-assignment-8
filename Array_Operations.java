1b4321abab1691b161d5dd88c2240dc2540d7154
tuannndt <tuann643123@gmail.com>
2024-09-06 19:24:33 +0700

    Array_Operations.java

--- Array_Operations.java
+++ Array_Operations.java
@@ -1,101 +1,54 @@
-
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
-public class Array_Operations {
+public class NumberExercise {
 
-    public static float[] importData(float[] arrays) {
+    public static ArrayList<Integer> EnterData(ArrayList<Integer> numbers) {
         Scanner scanner = new Scanner(System.in);
-        for (int i = 0; i < arrays.length; i++) {
-            System.out.print("Nhap phan tu thu " + (i + 1) + ": ");
-            arrays[i] = scanner.nextFloat();
-        }
-        return arrays;
-    }
+        System.out.println("Enter numbers (type 'done' to finish):");
 
-    public static void printData(float[] arrays) {
-        System.out.print("Mang hien tai: ");
-        for (float value : arrays) {
-            System.out.print(value + ", ");
-        }
-        System.out.println();
-    }
-
-    public static float findMax2(float[] arrays) {
-        float max1 = Float.MIN_VALUE;
-        float max2 = Float.MIN_VALUE;
-
-        for (float value : arrays) {
-            if (value > max1) {
-                max2 = max1;
-                max1 = value;
-            } else if (value > max2 && value != max1) {
-                max2 = value;
+        while (scanner.hasNext()) {
+            if (scanner.hasNextInt()) {
+                numbers.add(scanner.nextInt());
+            } else if (scanner.next().equalsIgnoreCase("done")) {
+                break;
+            } else {
+                System.out.println("Invalid input, please enter an integer or 'done' to finish.");
             }
         }
+        return numbers;
+    }
 
+    public static void DisplayData(ArrayList<Integer> numbers) {
+        System.out.println("Array contents: " + numbers);
+    }
+
+    public static int findMax2(ArrayList<Integer> numbers) {
+        if (numbers.size() < 2) {
+            throw new IllegalArgumentException("Array must contain at least two elements");
+        }
+        
+        int max1 = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+        
+        for (int num : numbers) {
+            if (num > max1) {
+                max2 = max1;
+                max1 = num;
+            } else if (num > max2 && num < max1) {
+                max2 = num;
+            }
+        }
+        
+        if (max2 == Integer.MIN_VALUE) {
+            throw new IllegalArgumentException("There is no second maximum value");
+        }
+        
         return max2;
     }
-
-    public static float[] deleteOddNumber(float[] arrays) {
-        int count = 0;
-        for (float value : arrays) {
-            if (value % 2 == 0) {
-                count++;
-            }
-        }
-
-        float[] newArray = new float[count];
-        int index = 0;
-        for (float value : arrays) {
-            if (value % 2 == 0) {
-                newArray[index++] = value;
-            }
-        }
-
-        return newArray;
-    }
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Nhap so luong phan tu cua mang: ");
-        int n = scanner.nextInt();
-        float[] arrays = new float[n];
-        boolean running = true;
-        while (running) {
-            System.out.println("\nMenu lua chon:");
-            System.out.println("1. Nhap du lieu vao mang");
-            System.out.println("2. In du lieu cua mang");
-            System.out.println("3. Tim gia tri lon thu 2 trong mang");
-            System.out.println("4. Xoa cac phan tu le trong mang");
-            System.out.println("5. Thoat");
-            System.out.print("Nhap lua chon cua ban: ");
-            int choice = scanner.nextInt();
-
-            switch (choice) {
-                case 1:
-                    importData(arrays);
-                    break;
-                case 2:
-                    printData(arrays);
-                    break;
-                case 3:
-                    float max2 = findMax2(arrays);
-                    System.out.println("Gia tri lon thu 2 trong mang la: " + max2);
-                    break;
-                case 4:
-                    arrays = deleteOddNumber(arrays);
-                    System.out.println("Mang sau khi xoa cac phan tu le:");
-                    printData(arrays);
-                    break;
-                case 5:
-                    running = false;
-                    System.out.println("Thoat chuong trinh.");
-                    break;
-                default:
-                    System.out.println("Lua chon khong hop le, vui long chon lai.");
-            }
-        }
-
-        scanner.close();
+    
+    public static void deleteOddNumber(ArrayList<Integer> numbers) {
+        numbers.removeIf(num -> num % 2 != 0);
     }
 }
